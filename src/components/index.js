bef71c099b9b0ea260948139386269af1064561f
Diarrah <diarrahcasiano@gmail.com>
2021-02-17 20:36:43 -0800

    Grouping for easy import

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Header } from './Header';
+export { default as About } from './About';
+export { default as Creations } from './Creations';
+export { default as Footer } from './Footer';
